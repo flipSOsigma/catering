a5114d5af568676ff4fad6d674020cbff9b13d3f
alif mahendra <nacubes20@gmail.com>
2025-05-07 16:17:12 +0700

    feat: making api for category

--- src/routes/get/orders-cattegory-get.ts
+++ src/routes/get/orders-cattegory-get.ts
@@ -2,7 +2,10 @@ import { Router } from "express";
 import prisma from "../../lib/prisma";
 const route = Router()
 
-export default route.get("/orders", async (req, res) => {
+export default route.get("/order/:category", async (req, res) => {
+  const { category } =  req.params
+  const capitalizedCategory = category.charAt(0).toUpperCase() + category.slice(1);
+
   try {
     const orders = await prisma.orderData.findMany({
       include: {
@@ -13,6 +16,11 @@ export default route.get("/orders", async (req, res) => {
             portions: true,
           },
         },
+      },
+      where: {
+        event: {
+          event_category: capitalizedCategory
+        }
       },
       orderBy: {
         created_at: "desc",
